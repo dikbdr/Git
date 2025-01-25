e6202d7d4839c4b3d89f3ac94198c20b13d6478f
Dik Katuwal <dikbdr@gmail.com>
2025-01-25 16:07:05 +0000

    updated sub func by b1

--- git_test.py
+++ git_test.py
@@ -1,3 +1,9 @@
-function add () {
-    pass
+a = int(input("Please enter first value"))
+b = int(input("Please enter second value"))
+def add() {
+    print(f "Sum of two value is {}" ,a+b)
+}
+
+def sub () {
+    print(f "Defference of two value is {}", a-b)
 }
