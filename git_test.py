44442ef3fb62adb1569018cd1c924af967a62775
Dik Katuwal <dikbdr@gmail.com>
2025-01-26 03:25:02 +0000

    Div func add by master

--- git_test.py
+++ git_test.py
@@ -12,6 +12,10 @@ class math_func:
        # print (first_value - second_value)
         print("Defference of two value is ", first_value - second_value)
     
+    def div (first_value, second_value):
+        d = first_value / second_value
+        print("Devision of two value is ", d)
+    
 a = int(input("Please enter first value"))
 b = int(input("Please enter second value"))
 c = math_func
