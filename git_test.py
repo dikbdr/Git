654b7e3bbacbe5525c9ea784dee341440149c305
Dik Katuwal <dikbdr@gmail.com>
2025-01-26 02:39:36 +0000

    update the class

--- git_test.py
+++ git_test.py
@@ -1,9 +1,18 @@
+
+class math_func(self, first_value, second_value):
+    self.first_value = first_value
+    self.second_value = second_value
+  
+    def add(first_value, second_value):
+        print (first_value + second_value)
+        # print(f "Sum of two value is {}" ,a+b)
+    
+    def sub (first_value, second_value):
+        print (first_value - second_value)
+    #  print(f "Defference of two value is {}", a-b)
+    
 a = int(input("Please enter first value"))
 b = int(input("Please enter second value"))
-def add() {
-    print(f "Sum of two value is {}" ,a+b)
-}
-
-def sub () {
-    print(f "Defference of two value is {}", a-b)
-}
+c = math_func
+c.add(a,b)
+c.sub(a,b)
